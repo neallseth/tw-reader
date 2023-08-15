f619208dc3964df3dc881d647fc8f9b8976c1ebc
neallseth <32910906+neallseth@users.noreply.github.com>
2023-08-14 21:31:39 -0700

    updating docs

--- index.js
+++ index.js
@@ -22,20 +22,36 @@ app.get("*", async (req, res) => {
     services: [
       {
         endpoint: "/tweet/{id}",
+        example: "/tweet/1359708562776330241",
         description:
           "Retrieve data for a particular tweet, along with its top replies - nested in their original tree structure",
+        status:
+          "active - though hierarchical replies no longer work (thanks elon :/)",
       },
       {
         endpoint: "/user/{handle}",
+        example: "/user/dril",
         description: "Retrieve a user's profile and account information",
+        status: "active",
       },
       {
         endpoint: "/user/{handle}/tweets",
+        example: "/user/dril/tweets",
         description: "Retrieve a user's recent non-reply tweets",
+        status: "active - works for most medium / large accounts",
       },
       {
         endpoint: "/user/{handle}/replies",
+        example: "/user/dril/replies",
         description: "Retrieve a user's recent tweets, including replies",
+        status: "deprecated - thanks elon :/",
+      },
+      {
+        endpoint: "/search?q={query}&start={yyyy-mm-dd}&end={yyyy-mm-dd}",
+        example: "/search?q=lk99",
+        description:
+          "Query recent tweets for a given string, optionally specify date range of tweet",
+        status: "active",
       },
     ],
   });
